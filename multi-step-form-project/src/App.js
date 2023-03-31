f0afaea73aefc88bd00087d09138d21938141c22
David Perez <perez.david3001@gmail.com>
2023-03-30 21:01:49 -0500

    react-initial push

---
+++ multi-step-form-project/src/App.js
@@ -0,0 +1,15 @@
+import {useState} from "react";
+import './App.css';
+import LeftContainer from './LeftContainer';
+import RightImage from './RightImage';
+
+export default function App() {
+  const [stage, setStage] = useState(0);
+
+  return (
+    <div className="App">
+      <LeftContainer stage={stage} setStage={setStage} />
+      <RightImage stage={stage} />
+    </div>
+  );
+}
